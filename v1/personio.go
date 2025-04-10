76847ff07874aa67c3064250b8d3ed754055cdd0
Jonas Zeiger <jonas@giantswarm.io>
2025-04-10 11:27:02 +0200

    fix linter errors (#83)

--- v1/personio.go
+++ v1/personio.go
@@ -50,11 +50,12 @@ type PersonioBool bool
 
 func (bit *PersonioBool) UnmarshalJSON(data []byte) error {
 	asString := string(data)
-	if asString == "1" || asString == "true" {
+	switch asString {
+	case "1", "true":
 		*bit = true
-	} else if asString == "0" || asString == "false" {
+	case "0", "false":
 		*bit = false
-	} else {
+	default:
 		return fmt.Errorf("boolean unmarshal error: invalid input %s", asString)
 	}
 	return nil
